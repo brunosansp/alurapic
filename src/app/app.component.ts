01cf4091a0a6aec882d439e40ba324668c4d9d28
Bruno Santos <brunosansp@outlook.com>
2020-10-01 08:21:33 -0300

    Add ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
@@ -6,13 +7,19 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Galeria de Fotos';
 
   photos: Object[] = [];
 
-  constructor(photoService: PhotoService) {
-    photoService.listFromUser('flavio')
-      .subscribe(photos => { this.photos = photos });
+  constructor(private photoService: PhotoService) {
   }
+
+  ngOnInit(): void {
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => { this.photos = photos });
+
+  }
+
 }
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
 import { Photo } from "./photo";
 
 const API = 'http://127.0.0.1:3000';
